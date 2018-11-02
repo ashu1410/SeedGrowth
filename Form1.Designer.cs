4e562836ae3f2cb214a2bbd9f31b1c222c5e2617
Mateusz Nogieć <ashajro@gmail.com>
2018-11-02 17:32:19 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace SeedGrowth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(93, 360);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 43);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Stop";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(205, 97);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Y";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(21, 97);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(100, 21);
+            this.comboBox2.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Sąsiedztwo";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(207, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Brzegowe";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(21, 163);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 19;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Rozmieszczenie ziaren";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(207, 135);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Liczba ziaren";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 360);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 43);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Zapisz bitmape";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(202, 213);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 13);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Ziarna oś Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Ziarna oś X";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(18, 266);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Promień";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(205, 283);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 27;
+            this.button3.Text = "Dodaj ziarna";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 29;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(210, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 30;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(205, 164);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 31;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(21, 243);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 32;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(205, 243);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 33;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(21, 299);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 34;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(277, 360);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 43);
+            this.button4.TabIndex = 35;
+            this.button4.Text = "Monte Carlo";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(356, 415);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Rozrost ziarna";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CellularAutomata;
+using System.Runtime.InteropServices;
+namespace SeedGrowth
+{
+    enum BoundaryConditions
+    {
+        Normal,
+        Periodic
+    }
+    enum Neighbourhood
+    {
+        Moore,
+        VonNoyman,
+        RandomPentagonal,
+        RandomHexagonal
+    }
+    enum SeedDraw
+    {
+        Random,
+        Evenly,
+       RandomWithRadius,
+        ByClick
+    }
+    public partial class Form1 : Form
+    {
+         SeedGrowth seedgtowth = null;
+        MonteCarlo monte = null;
+         Form2 simulationDisplayer;
+        BackgroundWorker worker = new BackgroundWorker();
+        int width;
+        int height;
+        List<String> bc = new List<String>();
+        List<String> nh = new List<String>();
+        List<String> sd = new List<String>();
+        neigbhourhoodType neigbhourhoodType;
+        internal SeedGrowth Automata
+        {
+            get { return seedgtowth; }     
+            set { seedgtowth = value; }
+        }
+
+        internal neigbhourhoodType NeigbhourhoodType { get {return neigbhourhoodType; } set { neigbhourhoodType = value; } }
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+            foreach (var condition in Enum.GetNames(typeof(BoundaryConditions)))
+                bc.Add(condition);
+            foreach (var condition in Enum.GetNames(typeof(Neighbourhood)))
+                nh.Add(condition);
+            foreach (var condition in Enum.GetNames(typeof(SeedDraw)))
+                sd.Add(condition);
+
+            comboBox1.DataSource = new BindingSource(bc, null);
+            comboBox2.DataSource = new BindingSource(nh, null);
+            comboBox3.DataSource = new BindingSource(sd, null);
+        }
+
+        private void PictureBox1_MouseClick1(object sender, MouseEventArgs e)
+        {
+            int x = Convert.ToInt32(e.X );
+            int y = Convert.ToInt32(e.Y );
+            ((SeedGrowth)(seedgtowth)).setSeed(x, y);
+            refreshView2();
+        }
+
+        private  void button1_Click(object sender, EventArgs e)
+        {
+           
+            {
+                if (worker.IsBusy)
+                    seedgtowth?.stop();
+
+
+                simulationDisplayer?.Dispose();
+                width = Convert.ToInt32(textBox1.Text == "" ? "300" : textBox1.Text);
+                height = Convert.ToInt32(textBox2.Text == "" ? "300" : textBox2.Text);
+                int seeds = Convert.ToInt32(textBox3.Text == "" ? "3" : textBox3.Text);
+
+                seedgtowth = new SeedGrowth(width, height);
+                
+
+                simulationDisplayer = new Form2();
+                simulationDisplayer.Size = new Size(width + 20, height + 45);
+                simulationDisplayer.pictureBox.Size = new Size(width, height);
+                simulationDisplayer.Disposed += Form2_Disposed;
+                simulationDisplayer.pictureBox.MouseClick += PictureBox1_MouseClick1;
+                switch ((String)comboBox3.SelectedItem)
+                {
+                    case "Random":
+
+                        seedgtowth.setSeedsRandomly(seeds);
+                        break;
+                    case "Evenly":
+                        int xasisseeds = Convert.ToInt32(textBox4.Text == "" ? "3" : textBox4.Text);
+                        int yaxisseeds = Convert.ToInt32(textBox5.Text == "" ? "3" : textBox5.Text);
+                        seedgtowth.setSeedsEvenly(xasisseeds, yaxisseeds);
+                        break;
+                    case "RandomWithRadius":
+                        int radius = Convert.ToInt32(textBox6.Text == "" ? "3" : textBox6.Text);
+                        seedgtowth.setSeedswithRadius(seeds, radius);
+                        break;
+                    case "ByClick":
+                        simulationDisplayer.pictureBox.MouseClick += PictureBox1_MouseClick1;
+                        break;
+                }
+
+
+                switch ((string)comboBox1.SelectedValue)
+                {
+                    case "Normal":
+                        switch ((string)comboBox2.SelectedValue)
+                        {
+                            case "Moore":
+                                neigbhourhoodType = neigbhourhoodType.Moore;
+                                break;
+                            case "VonNoyman":
+                                neigbhourhoodType = neigbhourhoodType.VonNoyman;
+                                break;
+                            case "RandomPentagonal":
+                                neigbhourhoodType = neigbhourhoodType.PentagonalRandom;
+                                break;
+                            case "RandomHexagonal":
+                                neigbhourhoodType = neigbhourhoodType.HexagonalRandom;
+                                break;
+                        }
+                        break;
+                    case "Periodic":
+                        switch ((string)comboBox2.SelectedValue)
+                        {
+                            case "Moore":
+                                neigbhourhoodType = neigbhourhoodType.MoorePeriodic;
+                                break;
+                            case "VonNoyman":
+                                neigbhourhoodType = neigbhourhoodType.VonNoymanPeriodic;
+                                break;
+                            case "RandomPentagonal":
+                                neigbhourhoodType = neigbhourhoodType.PentagonalRandomPeriodic;
+                                break;
+                            case "RandomHexagonal":
+                                neigbhourhoodType = neigbhourhoodType.HexagonalRandomPeriodic;
+                                break;
+                        }
+                        break;
+                }
+
+                button6.Text = "Stop";
+                simulationDisplayer.Show();
+
+
+
+                worker = new BackgroundWorker();
+                worker.DoWork += Worker_DoWork;
+                worker.WorkerSupportsCancellation = true;
+                worker.RunWorkerAsync();
+            }
+            
+
+        }
+
+        private void Form2_Disposed(object sender, EventArgs e)
+        {
+            seedgtowth?.stop();
+            monte?.stop();
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {                         
+            seedgtowth.Iterate(refreshView2, neigbhourhoodType );            
+        }
+
+        private void Worker_DoWork2(object sender, DoWorkEventArgs e)
+        {
+            monte.Iterate(refreshView3, neigbhourhoodType.Moore);
+        }
+
+
+        private void refreshView3()
+        {
+            Bitmap bitmap = new Bitmap(width, height);
+
+            // wersja rownoległa
+            var bitmapData = bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            bitmap.UnlockBits(bitmapData);
+            IntPtr ptr = bitmapData.Scan0;
+            Parallel.For(0, monte.Ibound, index =>
+            {
+
+                Color color;
+
+                for (int j = 0; j <= monte.Jbound; j++)
+                {
+
+                    int offset = index * 4 + j * bitmapData.Stride;
+                    color = Color.FromArgb(monte.Cells[index, j]);
+                    Marshal.WriteByte(ptr, offset, color.R);
+                    Marshal.WriteByte(ptr, offset + 1, color.G);
+                    Marshal.WriteByte(ptr, offset + 2, color.B);
+                    Marshal.WriteByte(ptr, offset + 3, color.A);
+
+
+                }
+            });
+            simulationDisplayer.pictureBox.Image = bitmap;
+        }
+        private void refreshView2()
+        {
+            Bitmap bitmap = new Bitmap(width, height);
+
+            // wersja rownoległa
+            var bitmapData = bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            bitmap.UnlockBits(bitmapData);
+            IntPtr ptr = bitmapData.Scan0;
+            Parallel.For(0, seedgtowth.Ibound, index =>
+            {
+
+                Color color;
+
+                for (int j = 0; j <= seedgtowth.Jbound; j++)
+                {
+
+                    int offset = index * 4 + j * bitmapData.Stride;
+                    color = Color.FromArgb(seedgtowth.Cells[index, j]);
+                    Marshal.WriteByte(ptr, offset, color.R);
+                    Marshal.WriteByte(ptr, offset + 1, color.G);
+                    Marshal.WriteByte(ptr, offset + 2, color.B);
+                    Marshal.WriteByte(ptr, offset + 3, color.A);
+
+
+                }
+            });
+            simulationDisplayer.pictureBox.Image = bitmap;
+
+            if (!areFreeCells())
+                seedgtowth.stop();
+        }
+
+        private bool areFreeCells()
+        {
+            foreach (int c in seedgtowth.Cells)
+                if (c == Color.Black.ToArgb())
+                    return true;
+            return false;
+        }
+          
+        private void button6_Click(object sender, EventArgs e)
+        {
+            switch(button6.Text)
+            {
+                case "Stop":
+                    Automata?.stop();
+                    button3.Enabled = true;
+                    button6.Text = Automata==null ? "Stop":"Run";
+                    break;
+                case "Run":
+                    worker.RunWorkerAsync();
+                    button3.Enabled = false;
+                    button6.Text = Automata == null ? "Run" : "Stop";
+                    break;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(!worker.IsBusy)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.AddExtension = true;
+                saveFileDialog.DefaultExt = ".bmp";
+                
+                if (saveFileDialog.ShowDialog()== DialogResult.OK)
+                {
+                    
+                    simulationDisplayer?.pictureBox.Image.Save(saveFileDialog.FileName, ImageFormat.Bmp);
+                    
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int seeds = Convert.ToInt32(textBox3.Text == "" ? "3" : textBox3.Text);
+            seedgtowth.setSeedsRandomly(seeds);
+            refreshView2();
+        }
+
+   
+
+        private void textBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            
+            width = Convert.ToInt32(textBox1.Text == "" ? "300" : textBox1.Text);
+            height = Convert.ToInt32(textBox2.Text == "" ? "300" : textBox2.Text);
+            int seeds = Convert.ToInt32(textBox3.Text == "" ? "3" : textBox3.Text);
+
+            monte = new MonteCarlo(width, height, seeds);
+
+
+            simulationDisplayer = new Form2();
+            simulationDisplayer.Size = new Size(width + 20, height + 45);
+            simulationDisplayer.pictureBox.Size = new Size(width, height);
+            simulationDisplayer.Disposed += Form2_Disposed;
+            //simulationDisplayer.pictureBox.MouseClick += PictureBox1_MouseClick1;
+            simulationDisplayer.Show();
+
+
+
+            worker = new BackgroundWorker();
+            worker.DoWork += Worker_DoWork2;
+            worker.WorkerSupportsCancellation = true;
+            worker.RunWorkerAsync();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
